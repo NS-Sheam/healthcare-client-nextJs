ac80be6b47663de884caa5c75d8aca572fc9e2fa
NS-Sheam <123sheamfeni@gmail.com>
2024-05-08 23:31:21 +0600

    chore: Add create specialty functionality and search bar to specialties page

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,9 +1,22 @@
+import PHModal from "@/components/Shared/PHModal/PHModal";
+import { Box, Button, Stack, TextField } from "@mui/material";
 
 const SpecialtiesPage = () => {
   return (
-    <div>
-      <h1>SpecialtiesPage</h1>
-    </div>
+    <Box>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Button>Create Specialty</Button>
+        <PHModal />
+        <TextField
+          size="small"
+          placeholder="Search Specialtist"
+        />
+      </Stack>
+    </Box>
   );
 };
 
