f7dc3f718ad2c3c0aa4343430fd2aeeef9edf64c
NS-Sheam <123sheamfeni@gmail.com>
2024-05-16 10:29:31 +0600

    updating profile

--- src/app/(withDashboardLayout)/dashboard/doctor/profile/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/profile/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useGetMYProfileQuery } from "@/redux/api/myProfile";
+import { useGetMYProfileQuery, useUpdateMYProfileMutation } from "@/redux/api/myProfile";
 import { Box, Container, Stack, Typography, styled } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import Image from "next/image";
 import DoctorInformation from "./components/DoctorInformation";
-
+import AutoFileUploader from "@/components/Forms/AutoFileUploader";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 const StyledInformationBox = styled(Box)(({ theme }) => ({
   background: "#f4f7fe",
   borderRadius: theme.spacing(1),
@@ -18,7 +19,19 @@ const StyledInformationBox = styled(Box)(({ theme }) => ({
 
 const Profile = () => {
   const { data, isLoading } = useGetMYProfileQuery(undefined);
-  console.log(data);
+  const [updateMyProfile, { isLoading: updating }] = useUpdateMYProfileMutation();
+  // console.log(data);
+
+  const fileUploadHandler = async (file: File) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("data", JSON.stringify({}));
+    try {
+      await updateMyProfile(formData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   if (isLoading) {
     return <Container>Loading...</Container>;
@@ -30,7 +43,10 @@ const Profile = () => {
         container
         spacing={2}
       >
-        <Grid xs={4}>
+        <Grid
+          xs={12}
+          md={4}
+        >
           <Box
             sx={{
               height: 300,
@@ -46,8 +62,22 @@ const Profile = () => {
               alt="profile"
             />
           </Box>
+          {updating ? (
+            <p>Updating...</p>
+          ) : (
+            <AutoFileUploader
+              name="file"
+              label="Upload Profile Photo"
+              icon={<CloudUploadIcon />}
+              onFileUpload={fileUploadHandler}
+              variant="text"
+            />
+          )}
         </Grid>
-        <Grid xs={8}>
+        <Grid
+          xs={12}
+          md={8}
+        >
           <DoctorInformation data={data} />
         </Grid>
       </Grid>
